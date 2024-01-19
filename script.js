4fa9adfb22421e2a247c5ede833ccebb9b5d9c1f
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-19 20:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function filterFnc(data) {
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			data=data.filter((item)=>item%2==0);
-			document.getElementById('output').innerHTML+=`${data.join(' ')}`;
 			resolve(data);
 		},1000)
 	})
@@ -23,14 +22,19 @@ function multiplyFnc(data) {
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			data.forEach((item,i)=>data[i]=item*2);
-			document.getElementById('output').innerHTML+=` ${data.join(' ')}`;
 			resolve(data);
 		},2000)
 	})
 }
 promiseFnc()
 	.then((data)=>filterFnc(data))
-	.then((data)=>multiplyFnc(data))
+	.then((data)=>{
+		document.getElementById('output').innerHTML+=`${data.join(' ')}`;
+		return multiplyFnc(data);
+	})
+	.then((data)=>{
+		document.getElementById('output').innerHTML+=` ${data.join(' ')}`;
+	})
 
 
 
